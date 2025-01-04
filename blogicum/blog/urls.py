0e9b21be0e82cd242543382a080fe8a4e4e9bc6f
Ira Lazarenko <lira_2005@bk.ru>
2025-01-05 01:20:14 +0600

    fixed url and post_detail adress

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,28 +6,39 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='index'),
+
     path('profile/<str:username>/',
          views.ProfileView.as_view(), name='profile'),
+
     path('accounts/profile/',
          views.ProfileEditView.as_view(), name='edit_profile'),
 
+
     path('', views.PostListView.as_view(), name='index'),
+
     path('posts/create/',
          views.PostCreateView.as_view(), name='create_post'),
+
     path('posts/<int:post_id>/edit/',
          views.PostUpdateView.as_view(), name='edit_post'),
+
     path('posts/<int:pk>/',
          views.PostDetailView.as_view(), name='post_detail'),
+
     path('posts/<post_id>/delete/',
          views.PostDeleteView.as_view(), name='delete_post'),
+
 
     path('category/<slug:category_slug>/',
          views.CategoryView.as_view(), name='category_posts'),
 
+
     path('posts/<int:post_id>/comment/',
          views.CommentCreateView.as_view(), name='add_comment'),
+
     path('posts/<int:post_id>/edit_comment/<int:comment_id>/',
          views.CommentUpdateView.as_view(), name='edit_comment'),
+
     path('posts/<int:post_id>/delete_comment/<int:comment_id>/',
          views.DeleteCommentView.as_view(), name='delete_comment'),
 ]
