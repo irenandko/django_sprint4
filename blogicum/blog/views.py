494ab85466a23e3fd215c82dc4e6cabd006b9f2c
Ira Lazarenko <lira_2005@bk.ru>
2025-01-05 01:14:51 +0600

    fixed post_detail

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -158,6 +158,11 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('blog:index')
     pk_url_kwarg = 'post_id'
 
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().author != request.user:
+            return redirect('blog:post_detail', self.kwargs['post_id'])
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = PostForm(instance=self.object)
